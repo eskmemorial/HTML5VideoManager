04404cf5db8bb146ecf31edf3720c651d69408c0
eskmemorial <54440061+eskmemorial@users.noreply.github.com>
2020-02-11 23:23:50 +0900

    changed DOM structure.

--- controller.js
+++ controller.js
@@ -13,10 +13,22 @@ function showController(video) {
     removeController();
 
     var controller = document.createElement("div");
+    controller.setAttribute("id", "controller");
+
+    var style = `
+position:fixed;
+top:10px;
+left:10px;
+z-index:1000;
+color:white;
+background:black;
+opacity:0.6;
+padding:5px;
+border-radius:3px;
+`;
+    controller.setAttribute("style", style);
     controller.innerHTML = `
-<div id="controller" style="position:fixed; top:10px; left:10px;color:white;background:black;opacity:0.6;padding:5px;border-radius:3px;">
     <div><button onclick="slowDown(document.querySelector('video'))">-</button>  ${format(video.playbackRate)}  <button onclick="accelerate(document.querySelector('video'))">+</button></div>
-</div>
     `;
 
     document.firstElementChild.appendChild(controller);
