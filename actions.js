461cf38453650864b1dd214d3fd88e180e66517f
eskmemorial <54440061+eskmemorial@users.noreply.github.com>
2020-02-11 13:14:04 +0900

    Restrict range of video speed,currentTime,volume

--- actions.js
+++ actions.js
@@ -7,31 +7,31 @@ function pause(video) {
 }
 
 function accelerate(video) {
-    video.playbackRate += 0.1;
+    video.playbackRate = Math.min(video.playbackRate + 0.1, 64);
 }
 
 function slowDown(video) {
-    video.playbackRate -= 0.1;
+    video.playbackRate = Math.max(video.playbackRate - 0.1, 0.1);
 }
 
 function resetSpeed(video) {
-    video.playbackRate = 1;
+    video.playbackRate = video.defaultPlaybackRate;
 }
 
 function advance(video) {
-    video.currentTime += 10;
+    video.currentTime = Math.min(video.currentTime + 10, video.duration - 1);
 }
 
 function rewind(video) {
-    video.currentTime -= 10;
+    video.currentTime = Math.max(video.currentTime - 10, 0);
 }
 
 function volumeUp(video) {
-    video.volume += 0.05;
+    video.volume = Math.min(video.volume + 0.05, 1);
 }
 
 function volumeDown(video) {
-    video.volume -= 0.05;
+    video.volume = Math.min(video.volume - 0.05, 0);
 }
 
 function resetVolume(video) {
