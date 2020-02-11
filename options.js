38a51868269680317005ba74a5beb3c40b7b385d
eskmemorial <54440061+eskmemorial@users.noreply.github.com>
2020-02-11 19:43:39 +0900

    Can save and load settings

--- options.js
+++ options.js
@@ -1,12 +1,33 @@
+var names = []
+
 document.querySelectorAll("input").forEach(input => {
+    names.push(input.getAttribute("name"));
+});
 
-    input.addEventListener("change", event => {
 
-        var key = event.target.getAttribute("name");
-        var value = event.target.value;
 
-        chrome.storage.sync.set({ key: value }, () => { });
 
+chrome.storage.sync.get(names, settings => {
+
+    names.forEach(name => {
+        document.querySelector(`input[name='${name}']`).setAttribute("value", settings[name]);
     });
+
+});
+
+
+
+
+
+document.querySelector("#save").addEventListener("click", event => {
+
+    var settings = {};
+
+    names.forEach(name => {
+
+        settings[name] = document.querySelector(`input[name='${name}']`).value;
+    });
+
+    chrome.storage.sync.set(settings, () => { });
 
 });
